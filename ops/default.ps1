3a2c76db2d0545704346a18532117ea759076587
Ruslan <ruslan.rusu@yahoo.com>
2017-02-16 23:06:26 -0500

    wip

---
+++ ops/default.ps1
@@ -0,0 +1,18 @@
+﻿
+properties {
+  $root = "C:\Users\Ruslan\Documents\byosb"
+  $sln = "$root\Dropin.UnicastBus.sln"
+}
+
+task default -depends Build
+
+task Build { 
+  
+	exec { msbuild $sln /p:Configuration=Release 
+                        /p:DebugType=None
+                        /p:Platform=AnyCpu
+                        /p:OutputPath=<path-to-package>
+                        /p:TargetProfile=Cloud
+                        /t:publish
+                        /verbosity:quiet }
+}
