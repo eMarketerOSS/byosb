01b4114cdd9fb61ef16f1d196d9e9781db11502c
Ruslan <ruslan.rusu@yahoo.com>
2017-03-01 00:39:00 -0500

    deployment wip

--- ops/default.ps1
+++ ops/default.ps1
@@ -8,10 +8,10 @@ properties {
   $sourceCscfgFile = "$root\host.inazure\ServiceConfiguration.Cloud.cscfg"
   $build = "$root\build"
   $cscfg = "$root\build\ServiceConfiguration.Cloud.cscfg"
-  $temp = "$root\build\temp"
+  $cspkg = "$root\build\host.inazure.cspkg"
 }
 
-task default -depends PrepareConfig
+task default -depends Deploy
 
 task PrepareConfig -depends BuildPackage {
 
@@ -46,4 +46,55 @@ task BuildPackage {
 
 	exec { msbuild $proj /p:Configuration=Release /p:PublishDir=$build\ /p:TargetProfile=Cloud /t:publish /verbosity:quiet /nologo }
 
+}
+
+task Deploy -depends PrepareConfig {
+
+	$cloudServiceName = "s100002"
+	$storageAccountName = "{0}storage" -f $cloudServiceName
+	$Location = "West US"
+
+	if (!(Test-AzureName -Storage $storageAccountName))
+	{  
+	    Write-Host "Creating Storage Account $storageAccountName"
+		New-AzureStorageAccount -StorageAccountName $storageAccountName -Location $Location -Verbose
+	}
+
+	if (!(Test-AzureName -Service $cloudServiceName))
+	{  
+	    Write-Host "Creating Cloud Service $cloudServiceName"
+		New-AzureService -ServiceName $cloudServiceName -Location $Location
+	}
+
+
+	#Add-AzureAccout and make it current
+
+	$s = Get-AzureSubscription -Current
+	Set-AzureSubscription -SubscriptionName $s.SubscriptionName -CurrentStorageAccount $storageAccountName
+
+
+	$deployment = $null
+	Try
+	{
+	    $deployment = Get-AzureDeployment -ServiceName $cloudServiceName
+	}
+	Catch
+	{
+	    New-AzureDeployment -ServiceName $cloudServiceName -Slot Production -Configuration $cscfg -Package $cspkg
+	}
+	If ($deployment)
+	{
+	    Set-AzureDeployment -ServiceName $cloudServiceName -Slot Production -Configuration $cscfg -Package $cspkg -Mode Simultaneous -Upgrade
+	}
+
+	# Mark the finish time of the script execution
+	$finishTime = Get-Date
+	# Output the time consumed in seconds
+	Write-Output ("Total time used (seconds): {0}" -f ($finishTime - $startTime).TotalSeconds)
+
+	# Launch the browser to show the website
+	If ($Launch)
+	{
+	    Start-Process -FilePath ("http://{0}.cloudapp.net" -f $cloudServiceName)
+	}
 }
